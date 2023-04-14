43332f23d5760a2fb877d32116ac931e2edcf550
jlmc <jlmc@froicorp.com>
2023-04-14 15:19:17 -0500

    .

---
+++ demo/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+///import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { Page404Component } from './core/componentes/page404/page404.component';
+import { InicioComponent } from './core/componentes/inicio/inicio.component';
+import { CoreModule } from './core/core.module'
+import { SharedModule } from './shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    AppRoutingModule,
+    CoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
